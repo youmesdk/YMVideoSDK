fb117d51458997bc692b72ace138e6058c188cfb
ci <zhuxingxing@mac.ci.nxit.us>
2018-06-05 14:03:01 +0800

    upgrade sdk to version:3.0.1.260

--- sample_ios/SDK/include/OpenGLView20.h
+++ sample_ios/SDK/include/OpenGLView20.h
@@ -13,56 +13,19 @@
 #import <OpenGLES/EAGL.h>
 #include <sys/time.h>
 
+typedef NS_ENUM(NSInteger, GLVideoRenderMode) {
+    GLVideoRenderModeHidden = 1,
+    GLVideoRenderModeFit = 2,
+    GLVideoRenderModeAdaptive = 3
+};
+
+
 @interface OpenGLView20 : UIView
-{
-	/** 
-	 OpenGL绘图上下文
-	 */
-    EAGLContext             *_glContext; 
-	
-	/** 
-	 帧缓冲区
-	 */
-    GLuint                  _framebuffer; 
-	
-	/** 
-	 渲染缓冲区
-	 */
-    GLuint                  _renderBuffer; 
-	
-	/** 
-	 着色器句柄
-	 */
-    GLuint                  _program;  
-	
-	/** 
-	 YUV纹理数组
-	 */
-    GLuint                  _textureYUV[3]; 
-	
-	/** 
-	 视频宽度
-	 */
-    GLuint                  _videoW;  
-	
-	/** 
-	 视频高度
-	 */
-    GLuint                  _videoH;
-    
-    GLsizei                 _viewScale;
-	   
-    //void                    *_pYuvData;
-    
-#ifdef DEBUG
-    struct timeval      _time;
-    NSInteger           _frameRate;
-#endif
-}
+
 #pragma mark - 接口
 - (void)displayYUV420pData:(void *)data width:(NSInteger)w height:(NSInteger)h;
 - (void)setVideoSize:(GLuint)width height:(GLuint)height;
-
+- (void)setRenderMode:(GLVideoRenderMode)mode;
 /** 
  清除画面
  */
